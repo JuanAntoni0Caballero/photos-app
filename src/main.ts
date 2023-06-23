ac35d3fc19f496192213d9d8abc9a7828233ca05
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-06-23 11:46:43 +0200

    environments

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
-
-
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
+
+if (environment.production) {
+  enableProdMode();
+}
+
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
